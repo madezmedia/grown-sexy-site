f36d015830a16aa594a02533e748e9ce39d8d82c
Mac <madezmediapartners@gmail.com>
2026-03-25 08:54:33 -0700

    fix: use correct Outstatic page setup with OstClient

--- app/outstatic/[[...ost]]/page.tsx
+++ app/outstatic/[[...ost]]/page.tsx
@@ -1,4 +1,12 @@
-import Outstatic from 'outstatic'
 import 'outstatic/outstatic.css'
+import { Outstatic } from 'outstatic'
+import { OstClient } from 'outstatic/client'
 
-export default Outstatic
+export default async function Page({
+  params,
+}: {
+  params: { ost?: string[] }
+}) {
+  const ostData = await Outstatic()
+  return <OstClient ostData={ostData} params={params} />
+}
