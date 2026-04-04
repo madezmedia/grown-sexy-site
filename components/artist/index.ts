77607e6897a4fda8c6ba18866cd9e0ed0723087d
Mac <madezmediapartners@gmail.com>
2026-04-04 13:00:56 -0700

    🐛 Remove unused artist index.ts causing TypeScript error

--- components/artist/index.ts
+++
@@ -1,8 +0,0 @@
-import { getFeaturedArtists } from "@/lib/artists";
-import { FeaturedArtistSection as FeaturedArtistSectionClient } from "./FeaturedArtistSection";
-
-export async function FeaturedArtistSection() {
-  const artists = getFeaturedArtists();
-  
-  return <FeaturedArtistSectionClient artists={artists} />;
-}
